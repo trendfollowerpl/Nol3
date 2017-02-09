739f6251ea0c0de2b9bf195e52c36d9536d4cc07
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-09 20:07:42 +0100

    tests and proj update

---
+++ Nol3.Communication.Unit.Tests/Nol3RegistryReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nol3.Communication;
+
+namespace Nol3.Communication.Unit.Tests
+{
+	[TestFixture]
+	public class Nol3RegistryReaderTests
+	{
+		[Test]
+		[Category("Registry Communication")]
+		[Description("Test if nol3 ionstallation returns bool")]
+		public void IsNolInstalled()
+		{
+			//arrange
+			var regReader = new Nol3RegistryReader();
+			//act
+			var actualValue = regReader.IsNol3Installed;
+			//assert
+			Assert.That(actualValue, Is.TypeOf(typeof(bool)));
+		}
+	}
+}
