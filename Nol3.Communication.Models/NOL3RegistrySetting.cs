79146d8f747c94f6ff4b3ef1120db26b9b6c792b
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-09 20:00:47 +0100

    Napisać moduł odczytywania wartości z rejestru do kumunikacji z NOLEM

--- Nol3.Communication.Models/NOL3RegistrySetting.cs
+++ Nol3.Communication.Models/NOL3RegistrySetting.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Nol3.Communication.Models
 {
-    public sealed class NOL3RegistrySetting
-    {
+	public sealed class NOL3RegistrySetting
+	{
 		private int _synchPort;
 		private int _asynchPort;
 
@@ -16,5 +16,13 @@ namespace Nol3.Communication.Models
 
 		public bool IsSynchPortActive { get; set; }
 		public bool IsAsynchPortActive { get; set; }
+	}
+
+	public static class Nol3RegistryKeys
+	{
+		public const string SynchPort = "nca_pasync";
+		public const string AsynchPort = "nca_psync";
+		public const string IsSynchPortActive = "ncaset_psync";
+		public const string IsAsynchPortActive = "ncaset_pasync";
 	}
 }
---
+++ Nol3.Communication/Nol3RegistryReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nol3.Communication.Interfaces;
+using Nol3.Communication.Models;
+using Microsoft.Win32;
+
+namespace Nol3.Communication
+{
+	public class Nol3RegistryReader : INOL3Configuration
+	{
+		public bool IsNol3Installed
+		{
+			get
+			{
+				return ReadRegistry(string.Empty) == null ? false : true;
+			}
+		}
+
+		public NOL3RegistrySetting Settings
+		{
+			get
+			{
+				return new NOL3RegistrySetting
+				{
+					SynchPort = ReadRegistry(Nol3RegistryKeys.SynchPort),
+					AsynchPort = ReadRegistry(Nol3RegistryKeys.AsynchPort),
+					IsSynchPortActive = Convert.ToBoolean(ReadRegistry(Nol3RegistryKeys.IsSynchPortActive)),
+					IsAsynchPortActive = Convert.ToBoolean(ReadRegistry(Nol3RegistryKeys.IsAsynchPortActive))
+				};
+			}
+		}
+
+		#region private
+		private const string registryPath = @"HKEY_CURRENT_USER\\Software\\COMARCH S.A.\\NOL3\\7\\Settings";
+
+		private int ReadRegistry(string valueName)
+		{
+			return (int)Registry.GetValue(registryPath, valueName, null);
+		}
+		#endregion
+	}
+}
