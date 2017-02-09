17e1eb2e805e760c2a14c704957fdf9f6f926a8d
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-09 19:11:11 +0100

    Models

---
+++ Nol3.Communication.Models/NOL3RegistrySetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nol3.Communication.Models
+{
+    public sealed class NOL3RegistrySetting
+    {
+		private int _synchPort;
+		private int _asynchPort;
+
+		public int SynchPort { get; set; }
+		public int AsynchPort { get; set; }
+
+		public bool IsSynchPortActive { get; set; }
+		public bool IsAsynchPortActive { get; set; }
+	}
+}
