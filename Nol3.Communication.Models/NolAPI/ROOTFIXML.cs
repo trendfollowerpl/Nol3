e9d002765ef11f568a33245ce3f21ad8c9637d0c
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-10 19:46:45 +0100

    podstawowe definicje obiektów

---
+++ Nol3.Communication.Models/NolAPI/ROOTFIXML.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Nol3.Communication.Models.NolAPI
+{
+	[XmlRoot(ElementName = "FIXML")]
+	public class ROOTFIXML<T> where T : new()
+	{
+		private string _v;
+		private string _r;
+		private string _s;
+		public static RequestType methodName;
+
+		public ROOTFIXML()
+		{
+			this.UserReq = new T();
+			v = "5.0";
+			r = "20080317";
+			s = "20080314";
+		}
+		[XmlElement]
+		public T UserReq { get; set; }
+
+		[XmlAttribute()]
+		public string v
+		{
+			get { return _v; }
+			set { _v = value; }
+		}
+		[XmlAttribute()]
+		public string r
+		{
+			get { return _r; }
+			set { _r = value; }
+		}
+
+		[XmlAttribute()]
+		public string s
+		{
+			get { return _s; }
+			set { _s = value; }
+		}
+	}
+}
---
+++ Nol3.Communication.Models/NolAPI/RequestType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nol3.Communication.Models.NolAPI
+{
+	public struct UserReqTyp
+	{
+		public static int Login = 1;
+		public static int Logout = 2;
+		public static int status = 4;
+	}
+}
