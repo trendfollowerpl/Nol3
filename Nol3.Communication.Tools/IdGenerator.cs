dbf4a717785ebe5b5ede59f31867aa178616e47b
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-11 13:55:26 +0100

    id generator

---
+++ Nol3.Communication.Tools/IdGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nol3.Communication.Tools
+{
+	public static class IdGenerator
+	{
+		private static int _id;
+		public static int GenerateId
+		{
+			get
+			{
+				_id++;
+				return _id;
+			}
+		}
+	}
+}
