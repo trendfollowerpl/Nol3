76fddab8e23bbc8ecc743771eb90f8efd86d6426
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-11 19:20:26 +0100

    UserRequest model

---
+++ Nol3.Communication.Models/NolAPI/UserRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Nol3.Communication.Models.NolAPI
+{
+	[XmlRoot(elementName: "UserReq")]
+	public class UserRequest
+	{
+		[XmlAttribute(attributeName: "UserReqID")]
+		public string UserRequestID { get; set; }
+		[XmlAttribute(attributeName: "UserReqTyp")]
+		public int UserRequestType { get; set; }
+		[XmlAttribute]
+		public int Username { get; set; }
+		[XmlAttribute]
+		public int Password { get; set; }
+	}
+}
--- Nol3.Communication.Unit.Tests/Tests/FIXMLTests.cs
+++ Nol3.Communication.Unit.Tests/Tests/FIXMLTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Nol3.Communication.Models.NolAPI;
 using System.Xml.Serialization;
 using Nol3.Communication.Unit.Tests.Test_Data;
+using Nol3.Communication.Tools;
 
 namespace Nol3.Communication.Unit.Tests
 {
@@ -31,6 +32,20 @@ namespace Nol3.Communication.Unit.Tests
 		{
 			string result = FIXML.GenerateRequest<EmptyTestClass>(new EmptyTestClass(), 1);
 			string expected = @"<FIXML v=""5.0"" r=""20080317"" s=""20080314""><UserReq /></FIXML>";
+			var sb = new StringBuilder();
+
+			sb.AppendLine(String.Format("RESULT     : {0}", result));
+			sb.AppendLine(String.Format("EXPECTED: {0}", expected));
+
+			TestContext.WriteLine(sb.ToString());
+			Assert.That(result, Is.EqualTo(expected));
+		}
+		[Test]
+		public void GenerateRequest_UserRequest()
+		{
+			IdGenerator.Reset();
+			string result = FIXML.GenerateRequest<UserRequest>(new UserRequest(),IdGenerator.ID);
+			string expected = @"<FIXML v=""5.0"" r=""20080317"" s=""20080314""><UserReq UserReqID="""" UserReqTyp=""1"" Username=""BOS"" Password =""BOS""/></FIXML>";
 			var sb = new StringBuilder();
 
 			sb.AppendLine(String.Format("RESULT     : {0}", result));
