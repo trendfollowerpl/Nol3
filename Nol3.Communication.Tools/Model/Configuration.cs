076bc2f7706296e9e70c5ab2f51a2ecec2090387
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-13 20:21:04 +0100

    1

---
+++ Nol3.Communication.Tools/Model/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Nol3.Communication.Tools.Model
+{
+	[XmlRoot]
+	public class Nol3Configuration
+	{
+		[XmlElement]
+		public int ID { get; set; }
+	}
+}
