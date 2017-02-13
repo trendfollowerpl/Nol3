7e8cb750c98c9c6ec26af7a2c12441be6ea3ab5c
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-13 20:08:03 +0100

    nol3connector singleton + test

---
+++ Nol3.Communication.Unit.Tests/Tests/Nol3ConnectorTests.cs
@@ -0,0 +1,30 @@
+﻿using Nol3.Communication.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nol3.Communication.Unit.Tests.Tests
+{
+	[TestFixture]
+	public class Nol3ConnectorTests
+	{
+		[Test]
+		public void TestIfSingletonWorksForNol3Client()
+		{
+			var settings = new NOL3RegistrySetting()
+			{
+				AsynchPort = 1,
+				IsAsynchPortActive = true,
+				IsSynchPortActive = true,
+				SynchPort = 2
+			};
+			var instance1 = Nol3Connector.CreateClient(settings);
+			var instance2 = Nol3Connector.CreateClient(settings);
+
+			Assert.That(instance1, Is.SameAs(instance2));
+		}
+	}
+}
---
+++ Nol3.Communication/Nol3Connector.cs
@@ -0,0 +1,33 @@
+﻿using Nol3.Communication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nol3.Communication
+{
+	public class Nol3Connector
+	{
+		private static Nol3Connector _Nol3ConnectorInstance = null;
+		private Socket _client;
+		private NOL3RegistrySetting _settings;
+
+		public Nol3Connector(NOL3RegistrySetting settings)
+		{
+			_settings = settings;
+			_client = null;
+		}
+
+		public static Nol3Connector CreateClient(NOL3RegistrySetting settings)
+		{
+			_Nol3ConnectorInstance = _Nol3ConnectorInstance != null
+				? _Nol3ConnectorInstance
+				: new Nol3Connector(settings);
+
+			return _Nol3ConnectorInstance;
+		}
+	}
+}
+
