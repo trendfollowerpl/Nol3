3a97aa77ea20dff02327fa5d5d623e60d4cf92ee
trendfollowerpl <trendfollowerpl@windowslive.com>
2017-02-26 10:51:07 +0100

    cd

--- Nol3.Communication.IntegrationTests/Nol3ConnectionTestsSetup.cs
+++ Nol3.Communication.IntegrationTests/Nol3ConnectionTestsSetup.cs
@@ -7,20 +7,24 @@ namespace Nol3.Communication.IntegrationTests
 	[TestFixture]
 	public partial class Nol3ConnectionTests
 	{
-		private Nol3Connector Nol3;
-		
+		public Nol3Connector Nol3Connector => Nol3Connector.CreateClient(Nol3RegistryReader.Settings);
+
 		[SetUp]
 		public void SetUp()
 		{
-			using (var _idGenerator = IdGenerator.GetIDGenerator())
-			{
-				Nol3ConfigurationManager.SaveConfiguration(new Tools.Model.Nol3Configuration
-				{
-					registryPath = Nol3ConfigurationManager.GetConfiguration().registryPath,
-					ID = Convert.ToInt32(_idGenerator.CurrentID)
-				});
-			}
-			Nol3 = Nol3Connector.CreateClient(Nol3RegistryReader.Settings);
+			Disposable.Using<IdGenerator, object>(
+				() => IdGenerator.GetIDGenerator(),
+				(_idGenerator) =>
+					{
+						Nol3ConfigurationManager.SaveConfiguration(new Tools.Model.Nol3Configuration
+						{
+							registryPath = Nol3ConfigurationManager.GetConfiguration().registryPath,
+							ID = Convert.ToInt32(_idGenerator.CurrentID)
+						});
+
+						return null;
+					}
+				);
 		}
 	}
 }
